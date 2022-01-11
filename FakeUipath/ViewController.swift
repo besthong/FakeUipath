1bcc10c89180211086df778b872d9e507cbda5fc
besthong <rjsekaskfro@naver.com>
2022-01-11 18:49:35 +0900

    Initial Commit

---
+++ FakeUipath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeUipath
+//
+//  Created by 훈쓰 on 2022/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //하이
+
+
+}
+
